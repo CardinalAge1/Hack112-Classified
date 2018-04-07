6a845a0b75043f0c9c4ec992d7a4994b8f2a2997
Abigail202113 <38144728+Abigail202113@users.noreply.github.com>
2018-04-07 15:23:41 -0400

    ehi

--- hough2.py
+++ hough2.py
@@ -5,62 +5,20 @@ import numpy as np
 
 video = cv.VideoCapture(0)
 
-tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
-tracker_type = tracker_types[2]
-if tracker_type == 'BOOSTING':
-    tracker = cv.TrackerBoosting_create()
-if tracker_type == 'MIL':
-    tracker = cv.TrackerMIL_create()
-if tracker_type == 'KCF':
-    tracker = cv.TrackerKCF_create()
-if tracker_type == 'TLD':
-    tracker = cv.TrackerTLD_create()
-if tracker_type == 'MEDIANFLOW':
-    tracker = cv.TrackerMedianFlow_create()
-if tracker_type == 'GOTURN':
-    tracker = cv.TrackerGOTURN_create()
-# Define an initial bounding box
-bbox = (287, 23, 86, 320)
-
-ok, frame = video.read()
-
-# Uncomment the line below to select a different bounding box
-bbox = cv.selectROI(frame, False)
-
-# Initialize tracker with first frame and bounding box
-ok = tracker.init(frame, bbox)
-
 while True:
     # Loads an image
     i, src = video.read()
-    ok, bbox = tracker.update(src)
+    # Check if image is loaded fine
     dst = cv.Canny(src, 50, 200, None, 3)
 
-    # Copy edges tos the images that will display the results in BGR
+    # Copy edges to the images that will display the results in BGR
     cdstP = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
 
     linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
-    if i:
-            # Tracking success
-        p1 = (int(bbox[0]), int(bbox[1]))
-        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-        cv.rectangle(src, p1, p2, (255, 0, 0), 2, 1)
-        cv.rectangle(cdstP, p1, p2, (255, 0, 0), 2, 1)
-    else:
-        # Tracking failure
-        cv.putText(src, "Tracking failure detected", (100, 80),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
-    # Display tracker type on frame
-    cv.putText(src, tracker_type + " Tracker", (100, 20),
-                cv.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
-
-    # Display FPS on frame
     if linesP is not None:
         for i in range(0, len(linesP)):
             l = linesP[i][0]
-            if (p1[0] < l[0] < p2[0] and p1[0] < l[2] < p2[0]) or\
-                (p1[1] < l[1] < p2[1] and p1[1] < l[3] < p2[1]): continue
             cv.line(src, (l[0], l[1]), (l[2], l[3]),
                     (0, 0, 255), 3, cv.LINE_AA)
             cv.line(cdstP, (l[0], l[1]), (l[2], l[3]),
@@ -70,6 +28,7 @@ while True:
     cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
 
     k = cv.waitKey(1) & 0xff
-    if k == 27: break
+    if k == 27:
+        break
 video.release()
 cv.destroyAllWindows()
--- solveMaze.py
+++ solveMaze.py
@@ -1,47 +1,99 @@
+import ourCozmo
+
+
 def isLeft(lines, left, right, orientation):
     wallDist = 50
-    if orientation == "up":
+    if orientation == 0:  # up
         for line in lines:
             if (min(line[0][1], line[1][1]) <= left[1]
                     <= max(line[0][1], line[1][1])):
                 if (0 <= left[0] - line[0][0] <= wallDist):
                     return True
-    if orientation == "down":
+    if orientation == 2:  # down
         for line in lines:
             if (min(line[0][1], line[1][1]) <= left[1]
                     <= max(line[0][1], line[1][1])):
                 if (0 <= line[0][0] - left[0] <= wallDist):
                     return True
+    if orientation == 3:  # left
+        for line in lines:
+            if (min(line[0][0], line[1][0]) <= left[0] <= max(line[0][0], line[1][0]) and
+                    0 <= left[1] - line[0][1] <= wallDist):
+                return True
+    elif orientation == 1:  # right
+        for line in lines:
+            if (min(line[0][0], line[1][0]) <= left[0] <= max(line[0][0], line[1][0]) and
+                    0 <= left[1] - line[0][1] <= wallDist):
+                return True
 
 
 def isRight(lines, left, right, orientation):
     wallDist = 50
-    if orientation == "up":
+    if orientation == 0:
         for line in lines:
             if (min(line[0][1], line[1][1]) <= right[1]
                     <= max(line[0][1], line[1][1])):
                 if (0 <= line[0][0] - right[0] <= wallDist):
                     return True
-    if orientation == "down":
+    if orientation == 2:
         for line in lines:
             if (min(line[0][1], line[1][1]) <= right[1]
                     <= max(line[0][1], line[1][1])):
                 if (0 <= right[0] - line[0][0] <= wallDist):
+                    return True
+    if orientation == 3:
+        for line in lines:
+            if (min(line[0][0], line[1][0]) <= right[0]
+                    <= max(line[0][0], line[1][0])):
+                if (0 <= right[1] - line[0][1] <= wallDist):
+                    return True
+    if orientation == 1:
+        for line in lines:
+            if (min(line[0][0], line[1][0]) <= right[0]
+                    <= max(line[0][0], line[1][0])):
+                if (0 <= line[0][1] - right[1] <= wallDist):
                     return True
 
 
 def isFront(lines, left, right, orientation):
     wallDist = 50
     middle = (left[0] + right[0] / 2, left[1] + right[1] / 2)
-    if orientation == "up":
+    if orientation == 0:
         for line in lines:
             if (min(line[0][0], line[1][0]) <= middle[0]
                     <= max(line[0][0], line[1][0])):
-                if (0 <= right[1] - line[0][1] <= wallDist):
+                if (0 <= middle[1] - line[0][1] <= wallDist):
                     return True
-    if orientation == "down":
+    if orientation == 2:
         for line in lines:
             if (min(line[0][0], line[1][0]) <= middle[0]
                     <= max(line[0][0], line[1][0])):
-                if (0 <= right[1] - line[0][1] <= wallDist):
+                if (0 <= line[0][1] - middle[1] <= wallDist):
                     return True
+    if orientation == 3:
+        for line in lines:
+            if (min(line[0][1], line[1][1]) <= middle[0]
+                    <= max(line[0][1], line[1][1])):
+                if (0 <= middle[0] - line[0][0] <= wallDist):
+                    return True
+    if orientation == 1:
+        for line in lines:
+            if (min(line[0][1], line[1][1]) <= middle[0]
+                    <= max(line[0][1], line[1][1])):
+                if (0 <= line[0][0] - middle[0] <= wallDist):
+                    return True
+
+
+def solveMazeLeft(lines, left, right, orientation):
+    if not isLeft(lines, left, right, orientation):
+        ourCozmo.run("left")
+        return (orientation - 1) % 4
+    elif not isFront(lines, left, right, orientation):
+        ourCozmo.run("straight")
+        return orientation
+    elif not isRight(lines, left, right, orientation):
+        ourCozmo.run("right")
+        return (orientation + 1) % 4
+    else:
+        ourCozmo.run("deadend")
+        return (orientation + 2) % 4
