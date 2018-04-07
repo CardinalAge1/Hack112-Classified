2e6357c6a892b341bb87caa6aee181e1ac476058
Daniel Garcia <danielga@andrew.cmu.edu>
2018-04-06 22:58:18 -0400

    Moved parallel running file to the top level

--- cozmo_sdk_examples_1.3.2/tutorials/01_basics/02_drive_and_turn.py
+++ cozmo_sdk_examples_1.3.2/tutorials/01_basics/02_drive_and_turn.py
@@ -18,18 +18,86 @@
 
 Make Cozmo drive forwards and then turn 90 degrees to the left.
 '''
-
+import pygame
 import cozmo
 from cozmo.util import degrees, distance_mm, speed_mmps
+pygame.init()
 
 
-def cozmo_program(robot: cozmo.robot.Robot):
-    # Drive forwards for 150 millimeters at 50 millimeters-per-second.
-    robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
+from tkinter import *
 
-    # Turn 90 degrees to the left.
-    # Note: To turn to the right, just use a negative number.
-    robot.turn_in_place(degrees(90)).wait_for_completed()
+####################################
+# customize these functions
+####################################
 
 
-cozmo.run_program(cozmo_program)
+def init(data):
+    # load data.xyz as appropriate
+    pass
+
+
+def mousePressed(event, data):
+    # use event.x and event.y
+    pass
+
+
+def keyPressed(event, data):
+    if event.keysym == "Up":
+        robot.drive_straight(distance_mm(10), speed_mmps(5000)
+                             ).wait_for_completed()
+
+
+def redrawAll(canvas, data):
+    # draw in canvas
+    pass
+
+####################################
+# use the run function as-is
+####################################
+
+
+def run(width=300, height=300):
+    def redrawAllWrapper(canvas, data):
+        canvas.delete(ALL)
+        canvas.create_rectangle(0, 0, data.width, data.height,
+                                fill='white', width=0)
+        redrawAll(canvas, data)
+        canvas.update()
+
+    def mousePressedWrapper(event, canvas, data):
+        mousePressed(event, data)
+        redrawAllWrapper(canvas, data)
+
+    def keyPressedWrapper(event, canvas, data):
+        keyPressed(event, data)
+        redrawAllWrapper(canvas, data)
+
+    # Set up data and call init
+    class Struct(object):
+        pass
+    data = Struct()
+    data.width = width
+    data.height = height
+    root = Tk()
+    init(data)
+    # create the root and the canvas
+    canvas = Canvas(root, width=data.width, height=data.height)
+    canvas.pack()
+    # set up events
+    root.bind("<Button-1>", lambda event:
+              mousePressedWrapper(event, canvas, data))
+    root.bind("<Key>", lambda event:
+              keyPressedWrapper(event, canvas, data))
+    redrawAll(canvas, data)
+    # and launch the app
+    root.mainloop()  # blocks until window is closed
+    print("bye!")
+
+
+run(400, 200)
+robot.drive_straight(distance_mm(10), speed_mmps(5000)
+                     ).wait_for_completed()
+
+# Turn 90 degrees to the left.
+# Note: To turn to the right, just use a negative number.
+robot.turn_in_place(degrees(9)).wait_for_completed()
